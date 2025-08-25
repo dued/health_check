a698bc500967986a26414cda367c089f65585f57
Cleb <clebaresu@gmail.com>
2025-08-25 16:22:16 -0500

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Health Check",
+    "summary": "Endpoint ligero para monitoreo de Odoo y DB",
+    "version": "18.0",
+    "author": "Cleb",
+    "license": "LGPL-3",
+    "category": "Monitoring",
+    "depends": ["base"],
+    "installable": True,
+    "application": False,
+}
